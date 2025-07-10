d0b2a36bbd65fb86f04703c956b3fd26b14a261f
kinguyen198 <67310086+kinguyen198@users.noreply.github.com>
2025-07-11 01:33:58 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,6 @@ var package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vmanot/Swallow.git", branch: "master"),
-        .package(url: "https://github.com/kinguyen198/swift-subprocessgit", branch: "main"),
     ],
     targets: [
         .target(
